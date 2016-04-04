dc8e4d9e6d58ad8df49d0d09de1b19d59a13062e
Erik Meier <erik_meier@discovery.com>
2016-04-03 23:20:25 -0500

    adds deploy task to fabfile
    deploy chains all the required commands together to init, create the env
    if necessary, and deploy the app

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,9 @@ import os
 
 from contextlib import contextmanager
 
-from fabric.api import task, local, prefix, env, execute
+from fabric.api import task, local, prefix, env, settings
 
-APP_NAME = 'sgdevops1'
+APP_NAME = 'sgdevops'
 APP_DESCRIPTION = """
 A sample application for use with Elastic Beanstalk
 """
@@ -32,12 +32,18 @@ def activate_this():
 
 @task
 def dev():
+    """
+    Sets the dev env vars
+    """
     env.eb_env_name = APP_NAME + '-dev'
     env.eb_scale = 1
 
 
 @task
 def prod():
+    """
+    Sets the prod env vars
+    """
     env.eb_env_name = APP_NAME + '-prod'
     env.eb_scale = 2
 
@@ -66,26 +72,51 @@ def server():
 
 @task
 def deploy():
-    execute(provision)
+    """
+    Deploys to Elastic Beanstalk, creating the environment if it does not exist.
+    """
+    eb_init(APP_NAME)
+
+    with settings(warn_only=True):
+        status = eb_status(env.eb_env_name)
+
+    if status.failed:
+        eb_create(env.eb_env_name)
+    else:
+        eb_deploy(env.eb_env_name)
 
 
 @task
-def eb_init(name, platform='python2.7'):
+def terminate():
+    """
+    Terminates an Elastic Beanstalk environment
+    """
+    eb_terminate(env.eb_env_name)
+
+
+@task
+def eb_init(name, platform='python2.7', region='us-west-2'):
     """
     Creates an Elastic Beanstalk application.
     """
     with virtualenv():
-        local('eb init -p {0} {1}'.format(platform, name))
+        local('eb init {0} -p {1} -r {2}'.format(name, platform, region))
 
 
 @task
-def eb_create_env(name=None):
-    cmd = 'eb create'
+def eb_create(name, cname=None):
+    """
+    Creates a new Elastic Beanstalk environment.
 
-    if name is not None:
-        cmd += ' ' + name
+    Throws an exception if the environment already exists.
+    """
+    if cname is None:
+        cname = name
 
-    local(cmd)
+    cmd = 'eb create {0} --cname {1}'.format(name, cname)
+
+    with virtualenv():
+        local(cmd)
 
 
 @task
@@ -98,12 +129,29 @@ def eb_list():
 
 
 @task
-def eb_deploy():
+def eb_status(name):
+    """
+    Gets Elastic Beanstalk environment information and status
+    """
+
+    cmd = 'eb status {0}'.format(name)
+
+    with virtualenv():
+        result = local(cmd, capture=True)
+
+    return result
+
+
+@task
+def eb_deploy(name):
     """
     Deploys to an Elastic Beanstalk environment
     """
+
+    cmd = 'eb deploy {0} -v'.format(name)
+
     with virtualenv():
-        local('eb deploy')
+        local(cmd)
 
 
 @task
