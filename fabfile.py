380d6df6154ff3649ac7adeb612731d734f07902
Erik Meier <erik_meier@discovery.com>
2016-04-03 23:22:26 -0500

    removes boto code

--- fabfile.py
+++ fabfile.py
@@ -22,14 +22,6 @@ def virtualenv():
         yield
 
 
-@contextmanager
-def activate_this():
-    activate_this = os.path.join(VENV_PATH, 'bin', 'activate_this.py')
-    execfile(activate_this, dict(__file__=activate_this))
-
-    yield
-
-
 @task
 def dev():
     """
@@ -175,63 +167,3 @@ def eb_scale(number, name=None):
 
     with virtualenv():
         local(cmd)
-
-
-@task
-def eb_delete_app(name=None, force=False):
-    if name is None:
-        name = APP_NAME
-
-    with activate_this():
-        import boto3
-
-        eb_client = boto3.client('elasticbeanstalk')
-
-        response = eb_client.delete_application(
-            ApplicationName=name,
-            TerminateEnvByForce=force
-        )
-
-        print(response)
-
-
-@task
-def provision():
-    """
-    Creates the Elastic Beanstalk application and environment if it does not already exist.
-    """
-    with activate_this():
-        import boto3
-
-        eb_client = boto3.client('elasticbeanstalk')
-
-        response = eb_client.describe_applications(
-            ApplicationNames=[APP_NAME]
-        )
-
-        if APP_NAME in [a['ApplicationName'] for a in response['Applications']]:
-            print(APP_NAME + ' exists, skipping creation.')
-        else:
-            print('Creating application: ' + APP_NAME)
-            response = eb_client.create_application(
-                ApplicationName=APP_NAME,
-                Description=APP_DESCRIPTION
-            )
-            print(response)
-
-        response = eb_client.describe_environments(
-            ApplicationName=APP_NAME,
-            EnvironmentNames=[env.eb_env_name]
-        )
-        print(response)
-        if env.eb_env_name in [e['EnvironmentName'] for e in response['Environments']]:
-            print(env.eb_env_name + ' exists, skipping creation.')
-        else:
-            print('Creating environment: ' + env.eb_env_name)
-            response = eb_client.create_environment(
-                ApplicationName=APP_NAME,
-                EnvironmentName=env.eb_env_name,
-                CNAMEPrefix=env.eb_env_name,
-                SolutionStackName='python2.7'
-            )
-            print(response)
